d905d5125f285b76d4213477aaa1e2c305461084
dr. wulsen <wula@wbox.lan>
2026-05-10 16:34:10 +0200

    commented out

--- components/amis/amis.cpp
+++ components/amis/amis.cpp
@@ -204,55 +204,53 @@ void amis::AMISComponent::amis_decode() {
           }
 		break;
         case 0xfb:
-			ESP_LOGD(TAG, "before 3.8.1");
           if(dif == 0x84 && dife == 0x10 && vife == 0x73) {
             // 3.8.1
  //           memcpy(&temp, &this->decode_buffer[i], data_len);
             ESP_LOGD(TAG, "3.8.1: %d", temp);
             reactive_energy_a_positive_sensor = 0;
-            if(this->reactive_energy_a_positive_sensor)
+//            if(this->reactive_energy_a_positive_sensor)
 //              // this->reactive_energy_a_positive_sensor->publish_state(temp);
-          }
-			ESP_LOGD(TAG, "after 3.8.1");
+//          }
           if(dif == 0x84 && dife == 0x10 && vife == 0x3c) {
             // 4.8.1
             //memcpy(&temp, &this->decode_buffer[i], data_len);
             ESP_LOGD(TAG, "4.8.1: %d", temp);
-            if(this->reactive_energy_a_negative_sensor)
+//            if(this->reactive_energy_a_negative_sensor)
  //             this->reactive_energy_a_negative_sensor->publish_state(temp);
-          }
-          if(dif == 0x04 && dife == 0x00 && vife == 0x14) {
+//          }
+//         if(dif == 0x04 && dife == 0x00 && vife == 0x14) {
             // 3.7.0
-            //memcpy(&temp, &this->decode_buffer[i], data_len);
+//            memcpy(&temp, &this->decode_buffer[i], data_len);
             ESP_LOGD(TAG, "3.7.0: %d", temp);
-            if(this->reactive_instantaneous_power_a_positive_sensor)
+//            if(this->reactive_instantaneous_power_a_positive_sensor)
 //              this->reactive_instantaneous_power_a_positive_sensor->publish_state(temp);
-          }
+//          }
           if(dif == 0x04 && dife == 0x00 && vife == 0x3c) {
             // 4.7.0
             //memcpy(&temp, &this->decode_buffer[i], data_len);
             ESP_LOGD(TAG, "4.7.0: %d", temp);
-            if(this->reactive_instantaneous_power_a_negative_sensor)
+//            if(this->reactive_instantaneous_power_a_negative_sensor)
 //              this->reactive_instantaneous_power_a_negative_sensor->publish_state(temp);
-          }
+//          }
         break;
         case 0x2b:
           if(dif == 0x04) {
             // 1.7.0
             //memcpy(&temp, &this->decode_buffer[i], data_len);
-            ESP_LOGD(TAG, "1.7.0: %d", temp);
-            if(this->instantaneous_power_a_positive_sensor)
+           ESP_LOGD(TAG, "1.7.0: %d", temp);
+//            if(this->instantaneous_power_a_positive_sensor)
 //              this->instantaneous_power_a_positive_sensor->publish_state(temp);
-          }
+//          }
         break;
         case 0xab:
           if(dif == 0x04 && vife == 0x3c) {
             // 2.7.0
             //memcpy(&temp, &this->decode_buffer[i], data_len);
             ESP_LOGD(TAG, "2.7.0: %d", temp);
-            if(this->instantaneous_power_a_negative_sensor)
+//            if(this->instantaneous_power_a_negative_sensor)
 //              this->instantaneous_power_a_negative_sensor->publish_state(temp);
-          }
+//          }
         break;
       }
       
