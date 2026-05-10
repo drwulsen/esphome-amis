79454f3f85e7423338a8ab31fda6c2ebb0405b75
dr. wulsen <wula@wbox.lan>
2026-05-10 18:14:38 +0200

    long reactive_negative

--- components/amis/amis.cpp
+++ components/amis/amis.cpp
@@ -204,6 +204,7 @@ void amis::AMISComponent::amis_decode() {
             // 3.8.1
             memcpy(&temp, &this->decode_buffer[i], data_len);
             ESP_LOGD(TAG, "3.8.1: %d", temp);
+            long reactive_energy_a_positive_sensor;
             if(this->reactive_energy_a_positive_sensor)
               ESP_LOGD(TAG, "reactive_energy_a_positive_sensor: %d", reactive_energy_a_positive_sensor);
 //              this->reactive_energy_a_positive_sensor->publish_state(temp);
