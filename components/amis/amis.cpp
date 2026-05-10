db8ede7c30969267e4a4711abd33c6c7e91cab50
dr. wulsen <wula@wbox.lan>
2026-05-10 16:13:31 +0200

    length

--- components/amis/amis.cpp
+++ components/amis/amis.cpp
@@ -201,7 +201,8 @@ void amis::AMISComponent::amis_decode() {
             // 2.8.0
             memcpy(&temp, &this->decode_buffer[i], data_len);
             uint32_t length = (decode_buffer[i], data_len);
-            ESP_LOGD(TAG, "2.8.0: %d", temp, length);
+            ESP_LOGD(TAG, "2.8.0: %d", temp);
+            ESP_LOGD(TAG, length);
             if(this->energy_a_negative_sensor)
               this->energy_a_negative_sensor->publish_state(temp);
           }
